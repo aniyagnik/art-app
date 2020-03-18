0106cdd35efe662b24d2d6794c0dbb366c5aedd6
aniyagnik <ani789yagnik@gmail.com>
2020-03-18 11:00:28 +0530

    adding js to access json file(testimonial)

--- public/script.js
+++ public/script.js
@@ -37,3 +37,28 @@ $(window).scroll(function(){
                 });
         }
 });
+
+$('.thumbnailImgs').click((e)=>{
+  let eventId=e.target.id
+  var thumbnailBg=$('#'+eventId).css('background-image')
+  thumbnailBg=thumbnailBg.split('')
+  let l1=thumbnailBg.length
+  thumbnailBg.splice(l1-1,l1-1)
+  thumbnailBg.splice(0,4)
+  thumbnailBg=thumbnailBg.join('')
+  console.log(thumbnailBg)
+  $('#mainImg').css('background-image',`url(${thumbnailBg})`);
+})
+
+$('#testiBullets').click((e)=>{
+  let eventId=e.target.id
+  
+  $.getJSON( "testimonials.json", function( data ) {
+    $.each( data, function( key, val ){
+      $('#testiPic').attr('src',data[eventId].image)
+      $('#testiText').text(data[eventId].testimonials)
+      $('#testiName').text(data[eventId].name)
+      $('#testiDesignation').text(data[eventId].designation)
+    });
+  });
+})
