53726e1d51e17bf3ef401518c188bb538f7d2260
aniyagnik <ani789yagnik@gmail.com>
2020-03-22 11:12:02 +0530

    dashboard - fixing frontend

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-document.getElementById('inputUsername').value=''
-document.getElementById('inputAddress').value=''
+
+let userValue=document.getElementById('userValue')
+let inputUsername=document.getElementById('inputUsername')
+let editName=document.getElementById('editName')
+let saveUsername=document.getElementById('saveUsername')
+
+let passwardValue=document.getElementById('passwardValue')
+let inputPassward=document.getElementById('inputPassward')
+let editPassward=document.getElementById('editPassward')
+let savePassward=document.getElementById('savePassward')
+
+let addressValue=document.getElementById('addressValue')
+let inputAddress=document.getElementById('inputAddress')
+let editAddress=document.getElementById('editAddress')
+let saveAddress=document.getElementById('saveAddress')
+
+let phoneValue=document.getElementById('phoneValue')
+let inputValue=document.getElementById('inputPhone')
+let editPhone=document.getElementById('editPhone')
+let savePhone=document.getElementById('savePhone')
+
+inputUsername.value=userValue.innerHTML
+inputPassward.value=''
+inputAddress.value=addressValue.innerHTML
+inputValue.value=phoneValue.innerHTML
+
 document.getElementById('cartList').addEventListener('click',(e)=>{
     console.log('ds')
     let eventId=e.target.id
@@ -13,35 +37,69 @@ document.getElementById('deleteCart').addEventListener('click',(e)=>{
     alert('cart emptied...')
 })
 
-document.getElementById('editName').addEventListener('click',(e)=>{
-    document.getElementById('userValue').style.display='none'
-    document.getElementById('inputUsername').style.display='block'
-    document.getElementById('editName').style.display='none'
-    document.getElementById('saveUsername').style.display='inline-block'    
+editName.addEventListener('click',(e)=>{
+    userValue.style.display='none'
+    inputUsername.style.display='block'
+    editName.style.display='none'
+    saveUsername.style.display='inline-block'    
 })
 
-document.getElementById('saveUsername').addEventListener('click',(e)=>{
-    document.getElementById('userValue').innerHTML=document.getElementById('inputUsername').value
-    document.getElementById('userValue').style.display='block'
-    document.getElementById('inputUsername').style.display='none'
-    document.getElementById('editName').style.display='inline'
-    document.getElementById('saveUsername').style.display='none'
-})
-
-document.getElementById('editAddress').addEventListener('click',(e)=>{
-    document.getElementById('addressValue').style.display='none'
-    document.getElementById('inputAddress').style.display='block'
-    document.getElementById('editAddress').style.display='none'
-    document.getElementById('saveAddress').style.display='inline-block'    
+saveUsername.addEventListener('click',(e)=>{    
+    userValue.innerHTML=inputUsername.value
+    userValue.style.display='block'
+    inputUsername.style.display='none'
+    editName.style.display='inline'
+    saveUsername.style.display='none'
 })
 
 
-document.getElementById('saveAddress').addEventListener('click',(e)=>{
-    document.getElementById('addressValue').innerHTML=document.getElementById('inputAddress').value
-    document.getElementById('addressValue').style.display='block'
-    document.getElementById('inputAddress').style.display='none'
-    document.getElementById('editAddress').style.display='inline'
-    document.getElementById('saveAddress').style.display='none'
+editPassward.addEventListener('click',(e)=>{
+    passwardValue.style.display='none'
+    inputPassward.style.display='block'
+    editPassward.style.display='none'
+    savePassward.style.display='inline-block'    
+})
+
+savePassward.addEventListener('click',(e)=>{    
+    alert('passward changed')
+    passwardValue.style.display='block'
+    inputPassward.style.display='none'
+    editPassward.style.display='inline'
+    savePassward.style.display='none'
+})
+
+
+editAddress.addEventListener('click',(e)=>{
+    addressValue.style.display='none'
+    inputAddress.style.display='block'
+    editAddress.style.display='none'
+    saveAddress.style.display='inline-block'    
+})
+
+
+saveAddress.addEventListener('click',(e)=>{
+    addressValue.innerHTML=inputAddress.value
+    addressValue.style.display='block'
+    inputAddress.style.display='none'
+    editAddress.style.display='inline'
+    saveAddress.style.display='none'
+})
+
+
+editPhone.addEventListener('click',(e)=>{
+    phoneValue.style.display='none'
+    inputValue.style.display='block'
+    editPhone.style.display='none'
+    savePhone.style.display='inline-block'    
+})
+
+
+savePhone.addEventListener('click',(e)=>{
+    phoneValue.innerHTML=inputValue.value
+    phoneValue.style.display='block'
+    inputValue.style.display='none'
+    editPhone.style.display='inline'
+    savePhone.style.display='none'
 })
 
 
